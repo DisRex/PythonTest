f08842a92b74c17fc7914301e1051cec525fa035
DisRex <psou@163.com>
2018-12-13 17:00:34 +0800

    函数定义，简单一元二次
    函数定义练习

---
+++ function/deffun.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 13 16:44:14 2018
+
+@author: D.S.Mix
+"""
+
+#请定义一个函数quadratic(a, b, c)，接收3个参数，返回一元二次方程：
+#ax2 + bx + c = 0的两个解。
+import math
+
+def quadratic(a,b,c):
+    result1=(-b+math.sqrt(pow(b,2)-4*a*c))/(2*a)
+    result2=(-b-math.sqrt(pow(b,2)-4*a*c))/(2*a)
+    return (result1,result2)
+
+
+
+
+# 测试:
+print('quadratic(2, 3, 1) =', quadratic(2, 3, 1))
+print('quadratic(1, 3, -4) =', quadratic(1, 3, -4))
+
+if quadratic(2, 3, 1) != (-0.5, -1.0):
+    print('测试失败')
+elif quadratic(1, 3, -4) != (1.0, -4.0):
+    print('测试失败')
+else:
+    print('测试成功')
