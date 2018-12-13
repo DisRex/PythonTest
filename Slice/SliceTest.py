563b2cd21c15181d44fb41e97269c024a4514cf9
DisRex <psou@163.com>
2018-12-13 17:23:32 +0800

    更新代码
    之前有乱码，更新代码

--- Slice/SliceTest.py
+++ Slice/SliceTest.py
@@ -5,39 +5,65 @@ Created on Thu Dec 13 14:37:39 2018
 @author: D.S.Mix
 """
 
-#������Ƭ������ʵ��һ��trim()������ȥ���ַ�����β�Ŀո�
+#利用切片操作，实现一个trim()函数，去除字符串首尾的空格
 #strTest = input()
 def trim1(s):
     l=len(s)
     i=0
     if l==0:
-        return ''  #����ַ�������Ϊ0��ֱ�ӷ��ؿ��ַ���
-    while s[i]==' ': #�ӵ�һ���ַ���ʼ�ж�
+        return ''  #如果字符串长度为0，直接返回空字符串
+    while s[i]==' ': #从第一个字符开始判断
         i=i+1
-        if i > l-1:  #�������ޣ�ֱ�ӷ��ؿ��ַ���
+        if i > l-1:  #超出界限，直接返回空字符串
             return ''
-    while s[l-1]==' ' : #�����һ���ַ���ʼ�ж�
+    while s[l-1]==' ' : #从最后一个字符开始判断
         l=l-1
-        if l-1 <= i:   #���λ�ý�������ʼ�ַ������Զ�ֹͣ
+        if l-1 <= i:   #如果位置将超过起始字符，则自动停止
             break
-    print('���룺'+s)
-    print('�����'+s[i:l])
+    print('输入：'+s)
+    print('输出：'+s[i:l])
     return s[i:l]
  
-# ���Ե�һ��:
+# 测试第一种:
 if trim1('hello  ') != 'hello':
-    print('1:��һ�β���ʧ��!')
+    print('1:第一次测试失败!')
 elif trim1('  hello') != 'hello':
-    print('1:�ڶ��β���ʧ��!')
+    print('1:第二次测试失败!')
 elif trim1('  hello  ') != 'hello':
-    print('1:�����β���ʧ��!')
+    print('1:第三次测试失败!')
 elif trim1('  hello  world  ') != 'hello  world':
-    print('1:���Ĵβ���ʧ��!')
+    print('1:第四次测试失败!')
 elif trim1('  h  ') != 'h':
-    print('1:����β���ʧ��!')
+    print('1:第五次测试失败!')
 elif trim1('') != '':
-    print('1:�����β���ʧ��!')
+    print('1:第六次测试失败!')
 elif trim1('    ') != '':
-    print('1:���ߴβ���ʧ��!')
+    print('1:第七次测试失败!')
 else:
-    print('1:���Գɹ�!')
+    print('1:测试成功!')
+
+#第二种 递归	
+def trim2(s):
+    if s[:1]==' ':      #判断第一个是否为空格
+        return trim(s[1:])
+    if s[-1:]==' ':     #判断最后一个是否为空格
+        return trim(s[:-1]) 
+    else: 
+        return s
+# 测试第二种:
+if trim2('hello  ') != 'hello':
+    print('2:第一次测试失败!')
+elif trim2('  hello') != 'hello':
+    print('2:第二次测试失败!')
+elif trim2('  hello  ') != 'hello':
+    print('2:第三次测试失败!')
+elif trim2('  hello  world  ') != 'hello  world':
+    print('2:第四次测试失败!')
+elif trim2('  h  ') != 'h':
+    print('2:第五次测试失败!')
+elif trim2('') != '':
+    print('2:第六次测试失败!')
+elif trim2('    ') != '':
+    print('2:第七次测试失败!')
+else:
+    print('2:测试成功!')
