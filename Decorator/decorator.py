bd1604532706587b94f58984aeec0ff6d0d62c08
DisRex <psou@163.com>
2018-12-17 13:47:42 +0800

    装饰器decorator
    装饰器练习

--- Decorator/decorator.py
+++ Decorator/decorator.py
@@ -6,27 +6,29 @@ Created on Sun Dec 16 22:45:41 2018
 """
 
 #请设计一个decorator，它可作用于任何函数上，并打印该函数的执行时间：
+#请编写一个decorator，能在函数调用的前后打印出'begin call'和'end call'的日志。
 
 import time, functools
-
 def metric(fn):
-        @functools.wraps(fn)
-        def wrapper(*args, **kw):
-            start = time.time()
-            result = fn(*args, **kw)
-            end = time.time()
-            print('%s executed in %s ms' % (fn.__name__, (end-start)/1000)) 
-            return result       
-        return wrapper
+    @functools.wraps(fn)
+    def wrapper(*args, **kw):
+        start = time.time()
+        print('begin call')
+        result = fn(*args, **kw)
+        end = time.time()
+        print('end call')
+        print('%s executed in %s ms' % (fn.__name__, (end-start)/1000)) 
+        return result       
+    return wrapper
 
 
 # 测试
-@metric
+@log('')
 def fast(x, y):
     time.sleep(0.0012)
     return x + y;
 
-@metric
+@log('slow:')
 def slow(x, y, z):
     time.sleep(0.1234)
     return x * y * z;
