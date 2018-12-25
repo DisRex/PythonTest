2cfaa0d5f000a5c5f10fd4439bdbd09dd825055e
DisRex <psou@163.com>
2018-12-25 10:47:16 +0800

    错误、单元测试
    错误、单元测试试验

---
+++ Error Debug Test/Error.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 25 09:19:10 2018
+
+@author: D.S.Mix
+"""
+
+from functools import reduce
+
+def str2num(s):
+    return float(s)
+
+def calc(exp):
+    ss = exp.split('+')
+    ns = map(str2num, ss)
+    return reduce(lambda acc, x: acc + x, ns)
+
+def main():
+    r = calc('100 + 200 + 345')
+    print('100 + 200 + 345 =', r)
+    r = calc('99 + 88 + 7.6')
+    print('99 + 88 + 7.6 =', r)
+
+main()
